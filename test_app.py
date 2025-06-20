64289b325b5277ae8aa0cb60c0d0405d0fedc6d7
Zniniz <shaheerqwi@gmail.com>
2025-06-20 19:33:26 -0400

    Playing a joke

--- test_app.py
+++ test_app.py
@@ -4,4 +4,4 @@ def test_home():
     client = app.test_client()
     resp = client.get('/')
     assert resp.status_code == 200
-    assert b'Docker CI/CD' in resp.data
+    assert b'If you are reading this you are all fatties!' in resp.data
