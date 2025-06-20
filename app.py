460508d69268c9891429564e00d7d046069251ac
Zniniz <shaheerqwi@gmail.com>
2025-06-20 10:47:31 -0400

    Changes to app.py

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, CI/CD World!"
-
+    return 'Hello, Docker CI/CD World!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
+
